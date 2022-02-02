0a0b4d73db1b9f026fcdb646429c63b4519565dc
LOSAFABIO <fabio.losa95@gmail.com>
2022-02-03 00:02:53 +0100

    ok trova film tramite api

---
+++ src/components/Main.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div class="streak">
+      <Cerca @filtra="getFilm"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Cerca from "./commons/Cerca.vue";
+import axios from "axios";
+
+export default {
+  name       : "Main",
+  components : {
+    Cerca
+  },
+  data(){
+    return {
+      apiURL    : "https://api.themoviedb.org/3/search/movie/",
+      filmArray : [],
+    }
+  },
+  methods:{
+    getFilm(select){
+        axios.get(this.apiURL, {
+          params : {
+            api_key : "a39afba29712edaa4c2865fea35ce64f",
+            query   : select
+          }
+        })
+        .then((risposta) => {
+          // handle success
+          this.filmArray = risposta.data.results;
+          console.log(this.filmArray)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.streak{
+  background-color: cadetblue;
+}
+
+
+</style>
---
+++ src/components/commons/Cerca.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input 
+    class="cerca" 
+    type="text" 
+    placeholder="Cerca quello che più ti interessa!"
+    v-model.trim="select" >
+
+    <button 
+    @click.prevent="$emit(`filtra`, select)" 
+    type="button">
+    Clicca qui!
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Cerca",
+    data(){
+        return{
+            select : ""
+        }
+    }
+}
+</script>
+
+<style>
+
+.cerca{
+    width: 220px;
+    margin: 20px;
+    border: none;
+    border-radius: 10px;
+    padding: 5px 5px;
+}
+
+button{
+    padding: 3px 15px;
+}
+
+</style>
